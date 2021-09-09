dd67c0674b58e3efd2533f735d3d98e8ad5058ee
Illia Rychenko <illia.rychenko1810@gmail.com>
2021-09-09 18:07:47 +0300

    Updating code for second homework

--- homework2/hw2.js
+++ homework2/hw2.js
@@ -20,10 +20,8 @@ const makeObjectDeepCopy = obj => {
     if (Array.isArray(arr) && Number(a) && Number(b) || a === 0 || b === 0) {
       for (let i = 0; i < arr.length; i++) {
         if (Number(arr[i]) || arr[i] === 0) {
-          if (a > b) {
-            if (arr[i] <= a && arr[i] >= b) {
+          if (a > b && arr[i] <= a && arr[i] >= b) {
               newArr.push(arr[i]);
-            }
           } else {
             if (arr[i] >= a && arr[i] <= b) {
               newArr.push(arr[i]);
